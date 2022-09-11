5944fd91091bdcd68e7e2950752eaacef8d62efe
Fahd <fahdpno@gmail.com>
2022-09-11 15:54:50 +0530

    FEAT: Added Header component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,14 +3,14 @@ import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className="nav flex flex-jc-btwn flex-ai-center">
+    <nav className="navbar-tab flex flex-jc-btwn flex-ai-center">
       <div className="flex flex-ai-center">
         <i class="fa-brands fa-react nav-logo"></i>
         <h1 className="title">React</h1>
       </div>
-      <div className="text-align-right">
-        <h3 className="user-name">John Wick</h3>
-        <h5 className="user-role">Developer</h5>
+      <div className="text-align-right flex flex-d-column">
+        <p className="user-name">John Wick</p>
+        <p className="user-role">Developer</p>
       </div>
     </nav>
   );
